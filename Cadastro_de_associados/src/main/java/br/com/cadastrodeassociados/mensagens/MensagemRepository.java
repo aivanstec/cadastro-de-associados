51f273665138a3d54d8ff1176f1a4fcd71b845b5
Ivan Arcanjelo Junior <ivan10arcanjelo@gmail.com>
2025-11-04 20:39:00 -0300

    Delete Cadastro_de_associados directory

--- Cadastro_de_associados/src/main/java/br/com/cadastrodeassociados/mensagens/MensagemRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.cadastrodeassociados.mensagens;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
-}
--- Cadastro_de_associados/src/main/java/br/com/cadastrodeassociados/pagamento/PagamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.cadastrodeassociados.pagamento;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-    List<Pagamento> findByAssociadoIdOrderByDataPagamentoDesc(Long associadoId);
-}
